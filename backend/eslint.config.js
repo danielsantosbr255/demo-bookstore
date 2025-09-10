1360f187b2760760b8ee1f6c0d2614ed2dfcec5d
Daniel Santos <danielsantosbr255@gmail.com>
2025-09-10 02:49:17 -0300

    chore(backend): update project configuration and enhance linting setup
    - Updated commitlint configuration for improved formatting.
    - Modified package.json to include lint-staged for pre-commit hooks and added new linting scripts.
    - Introduced ESLint and Prettier configurations for consistent code style in the backend.
    - Added TypeScript configuration for better type checking and compilation settings.
    - Created .prettierignore to exclude build artifacts from formatting.
    - Updated package-lock.json to reflect new dependencies and versions.

---
+++ backend/eslint.config.js
@@ -0,0 +1,28 @@
+// import js from '@eslint/js';
+// import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  // {
+  //   files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+  //   plugins: { js },
+  //   extends: ["js/recommended"],
+  //   languageOptions: { globals: globals.node },
+  // },
+  // {
+  //   rules: {
+  //     "no-unused-vars": "warn",
+  //     "no-undef": "warn",
+  //   },
+  // },
+  // {
+  //   env: {
+  //     node: true,
+  //     jest: true,
+  //   },
+  // },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+]);
