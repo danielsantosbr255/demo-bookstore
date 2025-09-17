c982675e900e5a7443e4330940c33aa846e167b2
Daniel Santos <danielsantosbr255@gmail.com>
2025-09-16 21:00:32 -0300

    refactor(user): restructure User class and define UserProps interface

--- backend/src/modules/users/dto/User.ts
+++ backend/src/modules/users/dto/User.ts
@@ -1,15 +1,25 @@
-export class User {
-  constructor(
-    public name: string,
-    public email: string,
-    public password: string
-  ) {}
+export interface UserProps {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
 }
 
-// Schema para validação no banco de dados
-export const UserSchema = {
-  id: 'number',
-  name: 'string',
-  email: 'string',
-  password: 'string',
-};
+export type UserCreateProps = Omit<UserProps, 'id'>;
+export type UserUpdateProps = Partial<Omit<UserProps, 'id'>>;
+
+export class User {
+  private static idCounter = 1;
+
+  static create(name: string, email: string, password: string): UserProps {
+    return { id: this.idCounter++, name, email, password };
+  }
+
+  static update(data: UserUpdateProps): UserUpdateProps {
+    const updatedData: UserUpdateProps = {};
+    if (data.name !== undefined) updatedData.name = data.name;
+    if (data.email !== undefined) updatedData.email = data.email;
+    if (data.password !== undefined) updatedData.password = data.password;
+    return updatedData;
+  }
+}
