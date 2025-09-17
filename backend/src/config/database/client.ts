5c2b045be2173231d9bbf2254d5d8a4889a395f6
Daniel Santos <danielsantosbr255@gmail.com>
2025-09-17 00:10:42 -0300

    chore(backend): update lint-staged configuration to use npm scripts for linting and formatting
    - previously lint-staged was running on all files,.
    - now it is working correctly, running only on staged files

--- backend/src/config/database/client.ts
+++ backend/src/config/database/client.ts
@@ -1,19 +1,19 @@
-// import { MemoryAdapter } from './adapters/in-memory.adapter.ts';
-import { PostgresAdapter } from './adapters/postgres.adapter.js';
+import { MemoryAdapter } from './adapters/in-memory.adapter.ts';
+// import { PostgresAdapter } from './adapters/postgres.adapter.js';
 import { IDatabase } from './IDatabase';
 
 let dbInstance: IDatabase;
 
 export const initDb = async () => {
-  // dbInstance = new MemoryAdapter();
+  dbInstance = new MemoryAdapter();
 
-  dbInstance = new PostgresAdapter({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  });
+  // dbInstance = new PostgresAdapter({
+  //   user: process.env.DB_USER,
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   password: process.env.DB_PASSWORD,
+  //   port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  // });
   await dbInstance.connect();
 };
 
