fd756eafc9f4243c588f1db97c3980b408be5967
Daniel Santos <danielsantosbr255@gmail.com>
2025-09-26 00:01:48 -0300

    refactor(backend): change to memory database adpter

--- backend/src/config/database/index.ts
+++ backend/src/config/database/index.ts
@@ -1,21 +1,21 @@
 // database/index.ts
 import { IDatabase } from './IDatabase';
-// import { MemoryAdapter } from './in-memory/in-memory.adapter.ts';
-import { PostgresAdapter } from './postgres/postgres.adapter.js';
+import { MemoryAdapter } from './in-memory/in-memory.adapter.ts';
+// import { PostgresAdapter } from './postgres/postgres.adapter.js';
 
 let dbInstance: IDatabase | null = null;
 
 export const initDb = async (): Promise<void> => {
   if (dbInstance) return; // evita inicializar mais de uma vez
 
-  // dbInstance = new MemoryAdapter();
-  dbInstance = new PostgresAdapter({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  });
+  dbInstance = new MemoryAdapter();
+  // dbInstance = new PostgresAdapter({
+  //   user: process.env.DB_USER,
+  //   host: process.env.DB_HOST,
+  //   database: process.env.DB_NAME,
+  //   password: process.env.DB_PASSWORD,
+  //   port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  // });
 
   await dbInstance.connect();
 };
