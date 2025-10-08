08ee0e8533d2e41d5b32df1f0aebf79e7efb376d
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-07 22:42:35 -0300

    refactor(user): update `UserMapper` for `VO` compatibility and clarity

--- backend/src/modules/users/user.mapper.ts
+++ backend/src/modules/users/user.mapper.ts
@@ -1,19 +1,26 @@
-import { IUser } from '../dto/user.dto';
-import { User } from '../entities/user.entity';
+import { IUser, UpdateUserDTO } from './dto/user.dto';
+import { User } from './user.entity';
 
 export class UserMapper {
+  static toDatabaseUpdate(user: User, dto: UpdateUserDTO): UpdateUserDTO {
+    return {
+      ...(dto.name && { name: user.name }),
+      ...(dto.email && { email: user.email }),
+    };
+  }
+
   static toDatabase(user: User): IUser {
     return {
       id: user.id,
       name: user.name,
       email: user.email,
-      password: user.password,
+      password: user.password.hashed,
       createdAt: user.createdAt,
       updatedAt: user.updatedAt,
     };
   }
 
-  static toDTO(user: IUser) {
+  static toResponse(user: IUser) {
     return {
       id: user.id,
       name: user.name,
