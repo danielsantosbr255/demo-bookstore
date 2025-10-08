4670fd275039b3abcc88a878e465c407e998b67a
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-07 22:42:35 -0300

    refactor(user): integrate email and password `vos` into user entity

--- backend/src/modules/users/user.entity.ts
+++ backend/src/modules/users/user.entity.ts
@@ -1,18 +1,20 @@
+import { Email } from '@/common/value-objects/email.vo';
+import { Password } from '@/common/value-objects/password.vo';
 import { v7 as uuidv7 } from 'uuid';
-import { CreateUserDTO } from '../dto/user.dto';
+import { CreateUserDTO } from './dto/user.dto';
 
 export class User {
   private _id: string;
   private _name!: string;
   private _email!: string;
-  private _password!: string;
+  private _password!: Password;
   private _createdAt: Date;
   private _updatedAt: Date;
 
   private constructor(
     name: string,
     email: string,
-    password: string,
+    password: Password,
     id?: string,
     createdAt?: Date,
     updatedAt?: Date
@@ -20,38 +22,34 @@ export class User {
     this._id = id || uuidv7();
     this.updateName(name);
     this.updateEmail(email);
-    this.updatePassword(password);
+    this._password = password;
     this._createdAt = createdAt || new Date();
     this._updatedAt = updatedAt || new Date();
   }
 
-  public static create(data: CreateUserDTO): User {
-    const { name, email, password, id, createdAt, updatedAt } = data;
-    return new User(name, email, password, id, createdAt, updatedAt);
+  public static async create(data: CreateUserDTO): Promise<User> {
+    const { id, name, email, password, createdAt, updatedAt } = data;
+
+    const passwordHashed = await Password.create(password);
+
+    return new User(name, email, passwordHashed, id, createdAt, updatedAt);
   }
 
   public updateName(newName: string): void {
     if (!newName || newName.length < 3) {
-      throw new Error('A name for update is invalid.');
+      throw new Error('Name must be at least 3 characters long.');
     }
     this._name = newName;
     this._updatedAt = new Date();
   }
 
   public updateEmail(newEmail: string): void {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(newEmail)) {
-      throw new Error('Invalid email address.');
-    }
-    this._email = newEmail;
+    this._email = new Email(newEmail).value;
     this._updatedAt = new Date();
   }
 
-  public updatePassword(newPassword: string): void {
-    if (!newPassword || newPassword.length < 6) {
-      throw new Error('A password for update is invalid.');
-    }
-    this._password = newPassword;
+  public async updatePassword(newPassword: string): Promise<void> {
+    this._password = await Password.create(newPassword);
     this._updatedAt = new Date();
   }
 
@@ -64,7 +62,7 @@ export class User {
   public get email(): string {
     return this._email;
   }
-  public get password(): string {
+  public get password(): Password {
     return this._password;
   }
   public get createdAt(): Date {
