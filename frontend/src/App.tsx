e5461e1206712011fb14b7c607854435c824fdb6
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-15 00:02:22 -0300

    fix(front-router): fix pathname condition

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import Users from "./pages/Users";
+import Header from "./components/header/Header";
+import { Activity } from "react";
 
 function App() {
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
-    <main className="bg-gray-500 h-screen">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </main>
+    <div className="bg-gradient-to-t from-sky-100 to-sky-50 h-dvh min-h-screen">
+      <Activity mode={path === "/sign-in" ? "hidden" : "visible"}>
+        <Header />
+      </Activity>
+
+      <main className="flex flex-1 h-full">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
