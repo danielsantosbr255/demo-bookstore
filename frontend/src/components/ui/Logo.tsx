691ffc1dc9c5382a5aea4d4e6483e10aff758dbd
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-15 00:11:23 -0300

    refactor(front-logo): improve logo design

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -1,8 +1,12 @@
+import { FaBookReader } from "react-icons/fa";
+import { Link } from "react-router";
+
 function Logo() {
   return (
-    <div>
-      <strong>Bookstore</strong>
-    </div>
+    <Link to="/" className="flex items-center gap-2 text-primary hover:text-secondary hover:scale-105 transition-all">
+      <FaBookReader size={40} />
+      <strong className="text-2xl font-bold tracking-widest text-gradient">Bookstore</strong>
+    </Link>
   );
 }
 
