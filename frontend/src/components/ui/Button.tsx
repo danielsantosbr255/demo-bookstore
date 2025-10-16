407ac887337c09ab9e1073d2f252e72c75db4b2e
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-16 17:50:11 -0300

    refactor(front-button): improve variant styles and improve management
    - remove framer-motion
    - add align option

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,34 +1,39 @@
-import { motion, type HTMLMotionProps } from "motion/react";
 import { twMerge } from "tailwind-merge";
 
 const variants = {
   primary: `
-    relative px-6 py-3 text-white font-semibold rounded-xl
+    relative px-6 py-3 text-white border-highlight font-semibold rounded-xl
     bg-gradient-to-r from-primary via-secondary/90 to-secondary
     bg-[length:200%_200%] transition-all duration-500
     hover:bg-[position:100%_0%]
+    backdrop-blur-sm
   `,
   secondary: "bg-secondary text-white",
-  outline: "text-primary",
+  outline: "text-primary text-shadow-2xs text-shadow-black/50",
 };
 
-type ButtonProps = React.ComponentProps<"button"> &
-  HTMLMotionProps<"button"> & {
-    variant?: keyof typeof variants;
-  };
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: keyof typeof variants;
+  align?: "left" | "center" | "right" | "between";
+};
 
-const Button = ({ children, className, variant = "primary", ...props }: ButtonProps) => {
+const Button = ({ children, className, variant = "primary", align = "between", ...props }: ButtonProps) => {
   return (
-    <motion.button
-      className={twMerge(
-        "flex items-center justify-between py-3 px-4 rounded-xl hover:scale-101 font-medium cursor-pointer border border-b-3 border-highlight transition-all duration-300",
-        variants[variant],
-        className
-      )}
+    <button
+      className={twMerge("active:scale-98 hover:scale-102 ease-in hover:motion-rotate-out-1 transition-transform", className)}
       {...props}
     >
-      <span className="relative flex w-full justify-between">{children}</span>
-    </motion.button>
+      <span
+        className={twMerge(
+          `flex flex-1 items-center justify-${align} py-3 px-4 gap-2 rounded-xl 
+          font-medium cursor-pointer border border-b-4          
+          `,
+          variants[variant]
+        )}
+      >
+        {children}
+      </span>
+    </button>
   );
 };
 
