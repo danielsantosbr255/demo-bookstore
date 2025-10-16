a2f2194fe83a873169c1aee3a32c2b8fbfb0d36b
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-16 17:53:09 -0300

    style(frontend): improve homepage styles
    - add banner
    - add categories section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,25 @@
-import { ImBooks } from "react-icons/im";
+import HomeBanner from "../components/home/HomeBanner";
+import CategoriesCard from "../components/home/CategoriesCard";
+import { GiUfo, GiLovers, GiCastle, GiCavalry, GiBrokenHeartZone, GiCowled } from "react-icons/gi";
 
 const Home = () => {
   return (
-    <div className="flex w-full h-full bg-gradient-to-b from-red-100 to-white items-center justify-center text-3xl">
-      <h1 className="flex flex-col font-bold text-primary items-center">
-        <ImBooks size={120} />
-        Bem-vindo ao Bookstore
-      </h1>
-    </div>
+    <main className="flex flex-col w-full h-full bg-gradient-to-b from-highlight via-sky-100 to-white items-center text-3xl">
+      <HomeBanner />
+
+      <section className="flex flex-col w-full items-center justify-center pt-5 gap-5">
+        <h2 className="text-2xl font-bold text-primary">Categorias em destaque</h2>
+
+        <div className="flex w-full gap-4 justify-center items-center">
+          <CategoriesCard label="Fantasia" icon={<GiCastle size={30} />} />
+          <CategoriesCard label="Ficcão" icon={<GiUfo size={30} />} />
+          <CategoriesCard label="Romance" icon={<GiLovers size={30} />} />
+          <CategoriesCard label="Aventura" icon={<GiCavalry size={30} />} />
+          <CategoriesCard label="Drama" icon={<GiBrokenHeartZone size={30} />} />
+          <CategoriesCard label="Suspense" icon={<GiCowled size={30} />} />
+        </div>
+      </section>
+    </main>
   );
 };
 
