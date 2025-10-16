2792f56a9f11948e95bb17403732b823f525b31a
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-16 17:42:26 -0300

    style(front-header): add new icons to the navigation bar and improve the styles

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 const Header = () => {
   return (
-    <div className="links bg-white fixed rounded-2xl shadow flex w-full px-6 py-4 z-50 max-w-11/12 left-1/2 -translate-x-1/2 top-4">
+    <div className="links bg-white/90 backdrop-blur-xl fixed rounded-2xl px-8 py-3 z-50 border-b-3 border-highlight/90 shadow-md shadow-highlight/40 flex w-full max-w-11/12 left-1/2 -translate-x-1/2 top-4">
       <Navbar />
     </div>
   );
--- frontend/src/components/header/Navbar.tsx
+++ frontend/src/components/header/Navbar.tsx
@@ -1,5 +1,16 @@
 import { Link } from "react-router";
 import Logo from "../ui/Logo";
+import { AiOutlineLogin } from "react-icons/ai";
+import { FaRegHeart, FaShoppingBag, FaUserCircle } from "react-icons/fa";
+import type { JSX } from "react";
+
+const NavItem = ({ label, icon, href }: { label: string; icon: JSX.Element; href: string }) => {
+  return (
+    <Link to={href} className="flex flex-col items-center hover:text-secondary hover:scale-105 transition-all">
+      {icon} {label}
+    </Link>
+  );
+};
 
 const Navbar = () => {
   return (
@@ -8,9 +19,11 @@ const Navbar = () => {
         <Logo />
       </div>
 
-      <div className="flex gap-4 items-center text-lg font-semibold">
-        <Link to="/sign-in">SignIn</Link>
-        <Link to="/users">Perfil</Link>
+      <div className="flex gap-6 items-center text-xs font-semibold">
+        <NavItem label="Notificacões" icon={<FaRegHeart size={25} />} href="#" />
+        <NavItem label="Carrinho" icon={<FaShoppingBag size={25} />} href="#" />
+        <NavItem label="Entrar" icon={<AiOutlineLogin size={25} />} href="/sign-in" />
+        <NavItem label="Perfil" icon={<FaUserCircle size={25} />} href="/profile" />
       </div>
     </nav>
   );
