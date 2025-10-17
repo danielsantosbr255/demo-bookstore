625c4bf9166d141b5e504b3538b9fdf2f70591f1
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-17 16:48:30 -0300

    style(front-anim): add animations when entering the page
    - add smooth animation to the sign-in form, using `tailwind motion`
    - add animated border

--- frontend/src/components/auth/SignInForm.tsx
+++ frontend/src/components/auth/SignInForm.tsx
@@ -6,36 +6,38 @@ import Logo from "../ui/Logo";
 
 const SignInForm = () => {
   return (
-    <div className="bg-white border-b-3 border-primary/50 text-gray-700 flex flex-col gap-8 2xl:w-[35rem] h-10/12 py-15 px-8 xl:px-15 rounded-2xl shadow-lg">
-      <section className="flex justify-center items-center">
-        <Logo />
-      </section>
+    <div className="border-spin h-10/12 w-full 2xl:w-[35rem] shadow-lg">
+      <div className="bg-white w-full flex flex-col gap-8 h-full py-15  px-8 xl:px-15 rounded-2xl">
+        <section className="flex justify-center items-center">
+          <Logo />
+        </section>
 
-      <section className="flex flex-col">
-        <h1 className="text-2xl font-bold">Acesse sua conta</h1>
-        <p>Informe seu e-mail e senha para entrar</p>
-      </section>
+        <section className="flex flex-col motion-preset-slide-left">
+          <h1 className="text-2xl font-bold">Acesse sua conta</h1>
+          <p>Informe seu e-mail e senha para entrar</p>
+        </section>
 
-      <section className="flex flex-col">
-        <form className="flex flex-col gap-4">
-          <Input type="email" name="Email" placeholder="Insira o email" />
-          <Input type="password" name="Senha" placeholder="Insira a senha" />
+        <section className="flex flex-col motion-preset-slide-down-left-sm">
+          <form className="flex flex-col gap-4">
+            <Input type="email" name="Email" placeholder="Insira o email" />
+            <Input type="password" name="Senha" placeholder="Insira a senha" />
 
-          <Button className="mt-4 flex justify-between">
-            Acessar
-            <AiOutlineLogin size={25} />
+            <Button className="mt-4 flex justify-between">
+              Acessar
+              <AiOutlineLogin size={25} />
+            </Button>
+          </form>
+        </section>
+
+        <section className="flex flex-col gap-3 justify-end h-full">
+          <p>Ainda não possui uma conta?</p>
+
+          <Button variant="outline" className="flex justify-between">
+            Cadastrar
+            <PiUserCirclePlusFill size={25} />
           </Button>
-        </form>
-      </section>
-
-      <section className="flex flex-col gap-3 justify-end h-full">
-        <p>Ainda não possui uma conta?</p>
-
-        <Button variant="outline" className="flex justify-between">
-          Cadastrar
-          <PiUserCirclePlusFill size={25} />
-        </Button>
-      </section>
+        </section>
+      </div>
     </div>
   );
 };
