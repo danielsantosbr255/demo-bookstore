6990e4b4dcbe075fbe153e5b5b78ccc6aa62cbdc
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-17 16:57:12 -0300

    style(front-home): improve styles an add responsivity

--- frontend/src/components/home/CategoriesCard.tsx
+++ frontend/src/components/home/CategoriesCard.tsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
-type Props = {
+type Props = React.HTMLAttributes<HTMLButtonElement> & {
   label: string;
   icon: React.ReactNode;
 };
 
-const CategoriesCard = ({ label, icon }: Props) => {
+const CategoriesCard = ({ label, icon, className, ...props }: Props) => {
   return (
-    <button className="border-rotating hover:scale-102 transition-transform cursor-pointer aspect-video w-50 pb-1 shadow-lg rounded-2xl">
-      <div className="bg-white h-full w-full rounded-2xl flex flex-col items-center justify-center text-center text-lg text-primary">
+    <button
+      {...props}
+      className={twMerge(
+        "border-spin hover:scale-102 transition-transform cursor-pointer aspect-[5/4] lg:aspect-video w-50 shadow-lg",
+        className
+      )}
+    >
+      <div className="bg-white h-full w-full p-2 rounded-2xl flex flex-col items-center justify-center text-center text-sm lg:text-lg">
         {icon}
-        <p className="text-primary text-lg font-bold">{label}</p>
+        <p className="font-bold">{label}</p>
         <div className="w-1/2 h-1 bg-gradient-to-r from-primary via-accent to-secondary/50 rounded-2xl" />
       </div>
     </button>
