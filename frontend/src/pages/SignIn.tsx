a12b752dece3c3364b496cfce37eb567ad5e476a
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-17 17:03:51 -0300

    refactor(front-auth): clean sign-in component

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -4,10 +4,8 @@ import { BookstoreShowcase } from "../components/auth/Showcase1";
 
 const SignIn = () => {
   return (
-    <main className="lg:grid grid-cols-2 xl:grid-cols-[2fr_5fr_auto] h-dvh w-full px-4 2xl:px-50 gap-4 items-center">
-      <div className="hidden xl:block bg-sky-100/50 w-full h-10/12 rounded-3xl shadow-lg">
-        <BookstoreShowcase />
-      </div>
+    <main className="flex w-full h-dvh lg:grid grid-cols-2 xl:grid-cols-[2fr_5fr_auto] px-4 2xl:px-50 gap-4 items-center">
+      <BookstoreShowcase />
       <Showcase />
       <SignInForm />
     </main>
