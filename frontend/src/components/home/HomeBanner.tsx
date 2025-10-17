af8a9209a750c3637dd421b3093e788047ca8342
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-17 16:58:27 -0300

    style(front-home): improve style an add animations

--- frontend/src/components/home/HomeBanner.tsx
+++ frontend/src/components/home/HomeBanner.tsx
@@ -4,17 +4,23 @@ import { GiSpellBook } from "react-icons/gi";
 
 const HomeBanner = () => {
   return (
-    <section className="border-rotating pb-1 w-full h-180 rounded-b-2xl shadow-lg shadow-secondary/40">
+    <section className="border-spin w-full h-150 lg:h-180 shadow-lg shadow-secondary/40">
       <div className="bg-home-pattern bg-center bg-no-repeat bg-cover flex flex-col w-full h-full items-center justify-center rounded-b-2xl">
-        <h1 className="flex flex-col font-bold text-white items-center">
-          <GiSpellBook size={120} />
-          Bem-vindo ao Bookstore
-        </h1>
+        <div className="motion-preset-slide-up space-y-6">
+          <h1 className="flex flex-col font-bold text-white text-shadow-2xs items-center">
+            <GiSpellBook size={120} />
+            Bem-vindo ao Bookstore
+          </h1>
 
-        <Button variant="primary" className="mt-4 text-xl min-w-xs" align="center">
-          Esplore novos títulos
-          <HiOutlineBookOpen />
-        </Button>
+          <Button
+            variant="primary"
+            className="mt-4 text-xl min-w-xs motion-preset-seesaw-sm motion-preset-stretch-sm"
+            align="center"
+          >
+            Esplore novos títulos
+            <HiOutlineBookOpen />
+          </Button>
+        </div>
       </div>
     </section>
   );
