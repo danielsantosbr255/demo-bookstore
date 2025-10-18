21870d3d65ebd2ae21bcfa1dcbdefcd296fb36f2
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-18 11:59:20 -0300

    refactor(front-icons): add new icons

--- frontend/src/libs/utils/icons.tsx
+++ frontend/src/libs/utils/icons.tsx
@@ -1,14 +1,52 @@
-import { GiSpellBook } from "react-icons/gi";
+import {
+  GiArchiveResearch,
+  GiBookCover,
+  GiSpellBook,
+  GiUfo,
+  GiLovers,
+  GiCastle,
+  GiCavalry,
+  GiBrokenHeartZone,
+  GiCowled,
+} from "react-icons/gi";
 import { AiOutlineLogin } from "react-icons/ai";
-import { FaRegHeart, FaShoppingBag, FaUserCircle } from "react-icons/fa";
+import { FaBookReader, FaRegHeart, FaShoppingBag, FaUserCircle } from "react-icons/fa";
+import { LuCloudUpload, LuHistory, LuUserSearch } from "react-icons/lu";
+import { PiUserCirclePlusFill } from "react-icons/pi";
+import { LuBookOpen, LuUsers, LuStar } from "react-icons/lu";
 
-type IconProps = React.SVGProps<SVGSVGElement> & {
-  size: number;
+type IconProps = {
+  size?: number;
+  className?: string;
 };
 
-export const CartIcon = ({ size, ...props }: IconProps) => <FaShoppingBag size={size} {...props} />;
-export const HomeIcon = ({ size, ...props }: IconProps) => <GiSpellBook size={size} {...props} />;
-export const SignInIcon = ({ size, ...props }: IconProps) => <AiOutlineLogin size={size} {...props} />;
-export const ProfileIcon = ({ size, ...props }: IconProps) => <FaUserCircle size={size} {...props} />;
-export const NotificationsIcon = ({ size, ...props }: IconProps) => <FaRegHeart size={size} {...props} />;
-// GiEntryDoor
+export const LogoIcon = (props: IconProps) => <FaBookReader {...props} />;
+export const CartIcon = (props: IconProps) => <FaShoppingBag {...props} />;
+export const ProfileIcon = (props: IconProps) => <FaUserCircle {...props} />;
+export const SearchIcon = (props: IconProps) => <GiArchiveResearch {...props} />;
+export const NotificationsIcon = (props: IconProps) => <FaRegHeart {...props} />;
+
+export const SignInIcon = (props: IconProps) => <AiOutlineLogin {...props} />;
+export const SignUpIcon = (props: IconProps) => <PiUserCirclePlusFill {...props} />;
+
+export const HomeBannerIcon = (props: IconProps) => <GiSpellBook {...props} />;
+export const ExploreBookIcon = (props: IconProps) => <GiBookCover {...props} />;
+
+export const ExploreRegIcon = (props: IconProps) => <LuUserSearch {...props} />;
+export const HistoryIcon = (props: IconProps) => <LuHistory {...props} />;
+export const CloudIcon = (props: IconProps) => <LuCloudUpload {...props} />;
+export const StarIcon = (props: IconProps) => <LuStar {...props} />;
+
+export const BooksIcon = (props: IconProps) => <LuBookOpen {...props} />;
+export const UsersIcon = (props: IconProps) => <LuUsers {...props} />;
+
+// categories
+export const FantasyIcon = (props: IconProps) => <GiCastle {...props} />;
+export const RomanceIcon = (props: IconProps) => <GiLovers {...props} />;
+export const HorrorIcon = (props: IconProps) => <GiBrokenHeartZone {...props} />;
+export const MysteryIcon = (props: IconProps) => <GiCavalry {...props} />;
+export const FictionIcon = (props: IconProps) => <GiUfo {...props} />;
+export const PoetryIcon = (props: IconProps) => <GiCowled {...props} />;
+export const AdventureIcon = (props: IconProps) => <GiCavalry {...props} />;
+export const DramaIcon = (props: IconProps) => <GiBrokenHeartZone {...props} />;
+export const SuspenseIcon = (props: IconProps) => <GiCowled {...props} />;
