74b0ac629a6a54d76ef1284ab3c9d4f7983b50b2
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-28 17:11:03 -0300

    refactor(back-auth): implement user session authentication correctly

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,9 +1,15 @@
+import { getClientIp } from '@/common/utils/getClientIp';
 import { config } from '@/core/config';
 import { Request, Response } from 'express';
+import { SessionsService } from '../sessions/sessions.service';
+import { signInSchema, signOutSchema } from './auth.dto';
 import { AuthService } from './auth.service';
 
 export class AuthController {
-  constructor(private readonly service: AuthService) {}
+  constructor(
+    private readonly service: AuthService,
+    private readonly sessionsService: SessionsService
+  ) {}
 
   signUp = async (req: Request, res: Response) => {
     const createdUser = await this.service.signUp(req.body);
@@ -11,25 +17,37 @@ export class AuthController {
   };
 
   signIn = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
+    const { email, password } = signInSchema.parse(req.body);
     const userLogged = await this.service.signIn({ email, password });
 
-    res.cookie('userId', userLogged.id, {
+    const user_agent = req.headers['user-agent'] ?? 'Desconhecido';
+    const user_id = userLogged.id;
+    const ip = getClientIp(req);
+
+    const createdSession = await this.sessionsService.create({ user_id, user_agent, ip });
+
+    res.cookie('sessionId', createdSession.id, {
       httpOnly: true,
       secure: config.env === 'production',
       sameSite: 'strict',
       maxAge: 24 * 60 * 60 * 1000,
     });
 
-    res.json({ message: 'User logged successfuly!', data: userLogged.name });
+    res.json({
+      message: 'User logged successfuly!',
+      data: createdSession,
+    });
   };
 
   signOut = async (req: Request, res: Response) => {
-    const { id } = req.params as { id: string };
-    const user = this.service.signOut({ id });
+    const { session_id, user_id } = signOutSchema.parse({
+      session_id: req.cookies.sessionId,
+      user_id: req.user_id,
+    });
 
-    res.clearCookie('userId');
+    await this.sessionsService.deleteByUserId(session_id, user_id);
 
-    res.json({ message: 'User logged out successfuly!', data: user });
+    res.clearCookie('sessionId');
+    res.json({ message: 'User logged out successfuly!', data: session_id });
   };
 }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,16 +1,16 @@
-import { CustomError } from '@/common/utils/CustomError';
-import { CreateUserDTO } from '../users/dto/user.dto';
+import { HttpError } from '@/core/errors/HttpError';
+import { CreateUserDTO } from '../users/user.dto';
 import { User } from '../users/user.entity';
 import { UserMapper } from '../users/user.mapper';
 import UsersService from '../users/users.service';
-import { SignInDTO, SignOutDTO } from './dto/auth.dto';
+import { SignInDTO } from './auth.dto';
 
 export class AuthService {
   constructor(private readonly userService: UsersService) {}
 
   async signUp(data: CreateUserDTO) {
     const userExists = await this.userService.getByEmail(data.email);
-    if (userExists) throw new CustomError('User already exists!', 409);
+    if (userExists) throw HttpError.Conflict('User already exists!');
 
     const user = await this.userService.create(data);
     return user;
@@ -18,19 +18,15 @@ export class AuthService {
 
   async signIn(data: SignInDTO) {
     const existsUser = await this.userService.getByEmail(data.email);
-    if (!existsUser) throw new CustomError('Invalid credentials!', 401);
+    if (!existsUser) throw HttpError.Unauthorized('Invalid credentials!');
 
     const user = User.fromDatabase(existsUser);
     const isPasswordValid = await user.password.compare(data.password);
 
     if (!isPasswordValid) {
-      throw new CustomError('Invalid credentials!', 401);
+      throw HttpError.Unauthorized('Invalid credentials!');
     }
 
     return UserMapper.toResponse(UserMapper.toDatabase(user));
-  }
-
-  signOut(data: SignOutDTO) {
-    return { id: data.id, message: 'User logged out successfuly!' };
   }
 }
