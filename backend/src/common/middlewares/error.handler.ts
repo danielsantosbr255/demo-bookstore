ca1616dec40837b583b2b209420ac3bb58d002d4
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-30 12:15:40 -0300

    refactor(auth-service): streamline user creation and authentication logic

--- backend/src/common/middlewares/error.handler.ts
+++ backend/src/common/middlewares/error.handler.ts
@@ -1,6 +1,7 @@
 import { config } from '@/core/config';
+import { HttpError } from '@/core/errors/HttpError';
 import { NextFunction, Request, Response } from 'express';
-import { CustomError } from '../utils/CustomError';
+import { z } from 'zod';
 import logger from '../utils/logger';
 
 const formatStackTrace = (stack: string | undefined) => {
@@ -15,13 +16,20 @@ const formatStackTrace = (stack: string | undefined) => {
 const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (config.env !== 'production') {
     logger.error(`Path Request: ${req.method} ${req.originalUrl}`);
-    logger.error('Status:', err instanceof CustomError ? err.statusCode : 500);
+    logger.error('Status:', err instanceof HttpError ? err.statusCode : 500);
     logger.error('Message:', err.message);
     logger.error('Stack trace:', formatStackTrace(err.stack));
   }
 
-  if (err instanceof CustomError) {
+  if (err instanceof HttpError) {
     res.status(err.statusCode).json({ message: err.message, statusCode: err.statusCode });
+    next();
+    return;
+  }
+
+  if (err instanceof z.ZodError) {
+    const message = err.issues[0].message;
+    res.status(400).json({ message, statusCode: 400 });
     next();
     return;
   }
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
+import { Password } from '@/common/value-objects/password.vo';
 import { HttpError } from '@/core/errors/HttpError';
 import { CreateUserDTO } from '../users/user.dto';
-import { User } from '../users/user.entity';
 import { UserMapper } from '../users/user.mapper';
 import UsersService from '../users/users.service';
 import { SignInDTO } from './auth.dto';
@@ -9,24 +9,17 @@ export class AuthService {
   constructor(private readonly userService: UsersService) {}
 
   async signUp(data: CreateUserDTO) {
-    const userExists = await this.userService.getByEmail(data.email);
-    if (userExists) throw HttpError.Conflict('User already exists!');
-
-    const user = await this.userService.create(data);
-    return user;
+    const createdUser = await this.userService.create(data);
+    return createdUser;
   }
 
   async signIn(data: SignInDTO) {
     const existsUser = await this.userService.getByEmail(data.email);
     if (!existsUser) throw HttpError.Unauthorized('Invalid credentials!');
 
-    const user = User.fromDatabase(existsUser);
-    const isPasswordValid = await user.password.compare(data.password);
+    const isPasswordValid = await Password.compare(existsUser.password, data.password);
+    if (!isPasswordValid) throw HttpError.Unauthorized('Invalid credentials!');
 
-    if (!isPasswordValid) {
-      throw HttpError.Unauthorized('Invalid credentials!');
-    }
-
-    return UserMapper.toResponse(UserMapper.toDatabase(user));
+    return UserMapper.toResponse(existsUser);
   }
 }
