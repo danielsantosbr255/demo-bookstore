17aad5f8ecb049a48ae538f1a65d95e646a886b1
Daniel Santos <danielsantosbr255@gmail.com>
2025-10-30 11:53:08 -0300

    feat(back-password): enhance Password value object with proper error handling and static methods

--- backend/src/common/value-objects/password.vo.ts
+++ backend/src/common/value-objects/password.vo.ts
@@ -1,10 +1,12 @@
+import { HttpError } from '@/core/errors/HttpError';
 import argon from 'argon2';
 
 export class Password {
   private constructor(public readonly hashed: string) {}
 
   static async create(raw: string): Promise<Password> {
-    if (raw.length < 6) throw new Error('Password too short');
+    if (raw.length < 6) throw HttpError.BadRequest('Password too short');
+
     const hashed = await argon.hash(raw, { type: argon.argon2id });
     return new Password(hashed);
   }
@@ -13,7 +15,7 @@ export class Password {
     return new Password(hashed);
   }
 
-  async compare(raw: string): Promise<boolean> {
-    return await argon.verify(this.hashed, raw);
+  static async compare(hashed: string, raw: string): Promise<boolean> {
+    return await argon.verify(hashed, raw);
   }
 }
